017a85cedd0ec9f842cb4fbb8321591a2cbe98f3
ShemetOff <shemetoff.prod@gmai.com>
2025-03-01 11:43:24 +0300

    Initial Commit

---
+++ Lesson3GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson3GitHub
+//
+//  Created by Kirill Shemetov on 1.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
